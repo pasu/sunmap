2e91bcac8b0d365c44b950f7e10cf75241814ab0
iclient <iclient@iclienttekiMac-mini.local>
2013-05-22 17:44:37 +0800

    Initial Commit

---
+++ sunmap/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  sunmapAppController.h
+//  sunmap
+//
+//  Created by iclient on 13-5-22.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ sunmap/ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sunmap
+//
+//  Created by iclient on 13-5-22.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
