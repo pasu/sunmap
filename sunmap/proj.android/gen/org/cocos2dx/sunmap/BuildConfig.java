dedab68c2696c95030108fbf87ba03aa0284eeb4
pasu <bjfubjfu@gmail.com>
2014-04-18 16:09:37 +0800

    remove

--- sunmap/proj.android/gen/org/cocos2dx/sunmap/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.cocos2dx.sunmap;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- sunmap/proj.android/obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o: \
- /Users/iclient/Desktop/cocos2d-2.1rc0-x-2.1.2/sunmap/proj.android/../../cocos2dx/support/ccUtils.cpp \
- /Users/iclient/Desktop/cocos2d-2.1rc0-x-2.1.2/sunmap/proj.android/../../cocos2dx/support/ccUtils.h
-
-/Users/iclient/Desktop/cocos2d-2.1rc0-x-2.1.2/sunmap/proj.android/../../cocos2dx/support/ccUtils.h:
